8b44977ac3016d99f7328465301fd4af7044313e
YangYiming <2651575456@qq.com>
2025-12-03 21:20:19 +0800

    开始做后端部分，初步创立数据库，然后完善了一些基础的类

---
+++ backend/src/main/java/cumt/kob/backend/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package cumt.kob.backend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import cumt.kob.backend.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+
+}
